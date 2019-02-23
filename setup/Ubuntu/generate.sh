0b71d2acb20d05060744ec99a7c8b3f539a392a5
undici77 <linuxale@email.it>
2019-02-23 17:49:01 +0100

    Fix Ubuntu deb generate.sh

--- setup/Ubuntu/generate.sh
+++ setup/Ubuntu/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cp ../../eInvoiceFreelance/bin/Release/eInvoiceFreelance.exe eInvoiceFreelance/opt/eInvoiceFreelance/eInvoiceFreelance.exe
+cp ../../eInvoiceFreelance/bin/Release/itextsharp.dll eInvoiceFreelance/opt/eInvoiceFreelance/itextsharp.dll
 dpkg-deb --build eInvoiceFreelance/
 
 
